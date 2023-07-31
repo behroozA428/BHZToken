3e7ac5f33c719d2a3746a2fe85b465b81d4ad60d
behroozA428 <97319411+behroozA428@users.noreply.github.com>
2023-07-31 23:40:02 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x88cbd72ec5c7fa7b48a84a797e029ad95e481f4d";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
